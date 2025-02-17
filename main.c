0dfd0b652097d5a3e9d6043abd946fca44904a52
MarkVologdin <vologdin.mark@gmail.com>
2025-02-17 10:30:25 -0800

    add main.c

---
+++ main.c
@@ -0,0 +1,133 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define NUM_EXPERIMENTS 6
+#define DATA_POINTS 100 // Количество точек данных в одном эксперименте
+#define TIME_START 0
+#define TIME_STOP 100
+#define M_PI 3.14159265358979323846
+
+#define RadianToDegree  (180./M_PI)
+#define D2R(Degrees)    ((Degrees)/RadianToDegree)          // Degree  --> Radians
+
+
+#define LATITUDE 55 // широта москвы в градусах 
+#define LONGITUDE 37 // долгота москвы в градусах 
+#define ALTITUDE 30 // высота москвы в метрах
+
+// Определяем константы для формулы Гельмерта (GRS80/WGS84)
+#define G_0 9.78030  // Ускорение свободного падения на экваторе (м/с^2)
+#define K 0.005302 // Безразмерный параметр
+#define E_2 0.000007 // Квадрат эксцентриситета Земли
+#define A 6378137.0 // Большая полуось Земли (м)
+
+double gravity_helmert(double latitude, double height) {
+    /*
+     * Вычисляет нормальную силу тяжести по формуле Гельмерта.
+     *
+     * :param latitude: Широта в градусах
+     * :param height: Высота над уровнем моря в метрах
+     * :return: Нормальная сила тяжести (м/с^2)
+     */
+    
+    double lat_rad = D2R(latitude); // Перевод широты в радианы
+
+    // Гравитация на уровне геоида (уровня моря) по формуле Гельмерта
+    double g_phi = G_0 * (1 + K * pow(sin(lat_rad), 2)) - E_2 * pow(sin(2 * lat_rad), 2);
+
+    // Коррекция на высоту h (аппроксимация)
+    double g_h = g_phi - (3.086 * 1e-6) * height;
+
+    return g_h;
+}
+
+// Структура для хранения одного считывания данных
+typedef struct {
+    double timestamp;  // Временная метка
+    double latitude;   // Широта (λ)
+    double longitude;  // Долгота (φ)
+    double altitude;   // Высота (h)
+    double heading;    // Курс (ψ)
+    double roll;       // Крен (θ)
+    double pitch;      // Тангаж (γ)
+    double f_z1;       // Сила f_z1
+    double f_z2;       // Сила f_z2
+    double f_z3;       // Сила f_z3
+} DataPoint;
+
+
+// Функция для инициализации данных эксперимента
+void initialize_data(DataPoint *data, int size, double start_heading_value, double stop_heading_value) {
+    double g_0 = gravity_helmert(LATITUDE, ALTITUDE);
+
+    for (int i = 0; i < size; i++) {
+        if(i == 0){
+            data[i].timestamp = 0.0;
+        }
+        else{
+            data[i].timestamp = data[i-1].timestamp + 0.01 ;
+        };
+        data[i].latitude = LATITUDE;
+        data[i].longitude = LONGITUDE;
+        data[i].altitude = ALTITUDE; 
+        data[i].heading = sigmoid(data[i].timestamp, start_heading_value, stop_heading_value, TIME_START, TIME_STOP);
+        data[i].roll = 0;
+        data[i].pitch = 0;
+        
+            
+    }
+}
+
+double sigmoid(double t, double start_heading, double stop_heading, double t_min, double t_max) {
+    // Центрируем сигмоиду в середине диапазона времени
+    double t0 = (t_min + t_max) / 2.0;
+    double k = 0.1;  // Коэффициент крутизны (чем больше, тем резче переход)
+
+    // Вычисляем значение сигмоиды
+    double sigmoid_value = 1.0 / (1.0 + exp(-k * (t - t0)));
+
+    // Масштабируем значение в диапазон [start_heading, stop_heading]
+    return start_heading + (stop_heading - start_heading) * sigmoid_value;
+}
+
+// Функция для генерации белого шума (нормального распределения)
+void generate_white_noise(DataPoint *data, int size) {
+    for (int i = 0; i < size; i++) {
+        data[i].value = data[i].value + ((double)rand() / RAND_MAX) * 2.0 - 1.0; // Шум в диапазоне [-1, 1]
+    }
+}
+
+// Функция для записи данных в файл
+void save_data_to_file(const char *filename, DataPoint *data, int size) {
+    FILE *file = fopen(filename, "w");
+    if (file == NULL) {
+        perror("Ошибка при открытии файла");
+        return;
+    }
+    
+    for (int i = 0; i < size; i++) {
+        fprintf(file, "%lf\n", data[i].value);
+    }
+    
+    fclose(file);
+}
+
+int main() {
+    srand(time(NULL)); // Инициализация генератора случайных чисел
+    
+    char filename[20];
+    DataPoint data[DATA_POINTS];
+    
+    for (int exp = 0; exp < NUM_EXPERIMENTS; exp++) {
+        initialize_data(data, DATA_POINTS);
+        generate_white_noise(data, DATA_POINTS);
+        
+        snprintf(filename, sizeof(filename), "experiment_%d.txt", exp + 1);
+        save_data_to_file(filename, data, DATA_POINTS);
+        
+        printf("Данные эксперимента %d записаны в %s\n", exp + 1, filename);
+    }
+    
+    return 0;
+}
